3f712dd3a2596c055cf4cbef9974f7580cfc511b
Mitchell Douglass <mrdouglass95@gmail.com>
2018-03-13 22:27:48 -0700

    remove ModelStart2

--- code/qa_model.py
+++ code/qa_model.py
@@ -216,11 +216,11 @@ class QAModel(object):
 
         with vs.variable_scope("ModelStart1"):
             model_start_encoder1 = LSTMEncoder(self.FLAGS.postatt_start_hidden_size, self.keep_prob)
-            model_start_reps_0 = model_start_encoder1.build_graph(blended_reps, self.context_mask)
+            model_start_reps = model_start_encoder1.build_graph(blended_reps, self.context_mask)
 
-        with vs.variable_scope("ModelStart2"):
-            model_start_encoder2 = LSTMEncoder(self.FLAGS.postatt_start_hidden_size, self.keep_prob)
-            model_start_reps = model_start_encoder2.build_graph(model_start_reps_0, self.context_mask)
+        #with vs.variable_scope("ModelStart2"):
+        #    model_start_encoder2 = LSTMEncoder(self.FLAGS.postatt_start_hidden_size, self.keep_prob)
+        #    model_start_reps = model_start_encoder2.build_graph(model_start_reps_0, self.context_mask)
 
         with vs.variable_scope("ModelEnd"):
             model_end_encoder = LSTMEncoder(self.FLAGS.postatt_end_hidden_size, self.keep_prob)

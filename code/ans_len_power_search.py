254c37cb4d896c963d296f033cc7bec08fca53fe
Mitchell Douglass <mrdouglass95@gmail.com>
2018-03-15 18:30:13 -0700

    add power search

---
+++ code/ans_len_power_search.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+import numpy as np
+
+if __name__ == '__main__':
+
+    os.chdir(os.path.join(os.path.dirname(__file__), '..'))
+
+    POWERS=np.arange(0.0, 0.21, 0.05)
+    F1={}
+    EM={}
+    best_f1 = 0.0
+    best_em = 0.0
+    for p in POWERS:
+        os.system(('python code/main.py --ans_len_dist_power={} '
+                   '--preatt_hidden_size=150 '
+                   '--postatt_start_hidden_size=150 '
+                   '--postatt_end_hidden_size=100 '
+                   '--glove_set=large '
+                   '--context_len=300 '
+                   '--mode=official_eval '
+                   '--json_in_path=data/dev-v1.1.json '
+                   '--ckpt_load_dir=experiments/character_embeddings')
+                   .format(p))
+        os.system('python code/evaluate.py data/dev-v1.1.json '
+                  'predictions.json > performance.json')
+        with open('performance.json', 'r') as f:
+            scores = json.load(f)
+        F1[p] = scores['f1']
+        EM[p] = scores['exact_match']
+        if F1[p] > F1[best_f1]:
+            best_f1 = p
+        if EM[p] > EM[best_em]:
+            best_em = p
+    with open('f1-1.json', 'w') as f:
+        json.dump(F1, f)
+    with open('em-1.json', 'w') as f:
+        json.dump(EM, f)
+    POWERS=np.arange(best_f1-0.04, best_f1+0.05, 0.01)
+    for p in POWERS:
+        if p in F1: continue
+        os.system(('python code/main.py --ans_len_dist_power={} '
+                   '--mode=official_eval '
+                   '--json_in_path=data/dev-v1.1.json '
+                   '--ckpt_load_dir=experiments/baseline/best_checkpoint')
+                   .format(p))
+        os.system('python code/evaluate.py data/dev-v1.1.json '
+                  'predictions.json > performance.json')
+        with open('performance.json', 'r') as f:
+            scores = json.load(f)
+        F1[p] = scores['f1']
+        EM[p] = scores['exact_match']
+        if F1[p] > F1[best_f1]:
+            best_f1 = p
+        if EM[p] > EM[best_em]:
+            best_em = p
+    with open('f1-2.json', 'w') as f:
+        json.dump(F1, f)
+    with open('em-2.json', 'w') as f:
+        json.dump(EM, f)
+    POWERS=np.arange(best_em-0.04, best_em+0.05, 0.01)
+    for p in POWERS:
+        if p in EM: continue
+        os.system(('python code/main.py --ans_len_dist_power={} '
+                   '--mode=official_eval '
+                   '--json_in_path=data/dev-v1.1.json '
+                   '--ckpt_load_dir=experiments/baseline/best_checkpoint')
+                   .format(p))
+        os.system('python code/evaluate.py data/dev-v1.1.json '
+                  'predictions.json > performance.json')
+        with open('performance.json', 'r') as f:
+            scores = json.load(f)
+        F1[p] = scores['f1']
+        EM[p] = scores['exact_match']
+        if F1[p] > F1[best_f1]:
+            best_f1 = p
+        if EM[p] > EM[best_em]:
+            best_em = p
+    with open('f1-3.json', 'w') as f:
+        json.dump(F1, f)
+    with open('em-3.json', 'w') as f:
+        json.dump(EM, f)
